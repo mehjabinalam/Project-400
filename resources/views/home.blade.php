0200574db25facc6904fda1fb21436b903f93199
DiptoAch <diptoach082@gmail.com>
2022-01-03 13:54:47 +0600

    mehjabin

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,7 +13,7 @@
                     </div>
                 @endif
 
-                {{ __('You are logged in!..') }}
+                {{ __('You are logged in!') }}
             </div>
         </div>
     </div>
